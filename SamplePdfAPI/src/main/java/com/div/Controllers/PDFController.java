cf2acdeed6a11d14b43f4cd5320b4b991cc43223
Divesh Bari <diveshbari1010@gmail.com>
2025-02-06 11:29:11 +0530

    sample pdf api which returns pdf file
    sample pdf api which returns pdf file

---
+++ SamplePdfAPI/src/main/java/com/div/Controllers/PDFController.java
@@ -0,0 +1,35 @@
+package com.div.Controllers;
+
+import java.io.IOException;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PDFController {
+	@GetMapping(path = "/getSamplePdf")
+	public ResponseEntity<byte[]> getSamplePdf() throws IOException {
+		System.out.println("getSamplePdf called");
+		
+		// Load the sample PDF file (Replace with actual file path)
+        ClassPathResource pdfFile = new ClassPathResource("Files/samplePDF.pdf");
+		
+        // Read file as byte array
+        byte[] pdfBytes = StreamUtils.copyToByteArray(pdfFile.getInputStream());
+        
+        // Set headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.setContentDisposition(ContentDisposition.attachment().filename("sample.pdf").build());
+        
+		return new ResponseEntity<>(pdfBytes, headers, HttpStatus.OK);
+	}
+	
+}
---
+++ SamplePdfAPI/src/main/java/com/div/SamplePdfApiApplication.java
@@ -0,0 +1,13 @@
+package com.div;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplePdfApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplePdfApiApplication.class, args);
+	}
+
+}
